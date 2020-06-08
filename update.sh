d39e08acdad50df939d8ffd6ac2deced153c3626
Lcx@ <1556461835@qq.com>
2020-06-08 14:38:20 +0800

    更新

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 ###
  # @Author: your name
  # @Date: 2020-06-08 14:31:00
- # @LastEditTime: 2020-06-08 14:31:17
+ # @LastEditTime: 2020-06-08 14:37:47
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: \iem-web-projectd:\个人工具网站\www_admin_master\updata.sh
 ### 
-git add . && git commit -m '更新' && git push origin master
+git pull origin master && git add . && git commit -m '更新' && git push origin master
